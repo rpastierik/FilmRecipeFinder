e44f1c7011591c084e0e671464b93d5a2992bdea
rpastierik <pastierik@gmail.com>
2026-03-22 08:42:53 +0100

    new version

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ class RecipeField:
 
 
 class Constants:
-    APP_VERSION = "0.5.6"
+    APP_VERSION = "0.5.7"
     SETTINGS_FILE = "user_settings.json"
     XML_FILE = "film_simulations.xml"
     
